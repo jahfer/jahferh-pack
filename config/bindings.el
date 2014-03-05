0a25fb543ce507963491836530f8d917267490db
Jahfer Husain <jahferh@gmail.com>
2014-03-05 09:35:11 -0500

    Added helm-ls-git-ls to keybindings

--- config/bindings.el
+++ config/bindings.el
@@ -1,7 +1,7 @@
 (setq mac-command-modifier 'super) ; sets the Command key to Meta
 
 ;; Helm
-(define-key global-map (kbd "C-c h") 'helm-mini)
+(define-key global-map (kbd "C-p") 'helm-ls-git-ls)
 
 ;; Buffers
 (define-key global-map (kbd "s-]") 'next-buffer)
