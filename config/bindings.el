f356d2744fe4acbbeafd7cbe26f11ce2d1cc9a28
Jahfer Husain <jahferh@gmail.com>
2014-03-05 09:02:53 -0500

    Set command key to super

--- config/bindings.el
+++ config/bindings.el
@@ -1,9 +1,8 @@
+(setq mac-command-modifier 'super) ; sets the Command key to Meta
+
 ;; Helm
 (define-key global-map (kbd "C-c h") 'helm-mini)
 
 ;; Buffers
 (define-key global-map (kbd "s-]") 'next-buffer)
 (define-key global-map (kbd "s-[") 'previous-buffer)
-
-;; Settings
-(define-key global-map (kbd "C-,") 'open-config-file)
